5b6a3c093ca12906515f1d51ed7c8849cb705d98
Ekaterina Shevchenko <ekaterina.shevchenko@finteco.io>
2022-07-18 15:23:18 +0300

    Refreshing

---
+++ Homework2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HomeWork2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int x = int.Parse(Console.ReadLine());
+            int factorial = 1;
+            for (int i = x; i > 1; i--)
+            {
+                factorial = factorial * i;
+            }
+            {
+                Console.WriteLine(factorial);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Homework3/HomeWorkCalc/HomeWork3/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace HomeWork3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Calculator");
+            Console.WriteLine("----------------------------");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            char oper = char.Parse(Console.ReadLine());
+            int c;
+            switch (char)
+            {
+            
+                case 1:
+                        {
+                        a = Console.ReadLine();
+                        b = Console.ReadLine();
+                        c = a + b;
+                        Console.WriteLine(c);
+                        break;
+                        }
+                case 2:
+                        {
+                        a = Console.ReadLine();
+                        b = Console.ReadLine();
+                        c = a - b;
+                        Console.WriteLine(c);
+                        break;
+                        }
+                case 3:
+                        {
+                        a = Console.ReadLine();
+                        b = Console.ReadLine();
+                        c = a * b;
+                        Console.WriteLine(c);
+                        break;
+                        }
+                case 4:
+                        {
+                        a = Console.ReadLine();
+                        b = Console.ReadLine();
+                        c = a / b;
+                        Console.WriteLine(c);
+                        break;
+                        }
+                default:
+                        {
+                            Console.WriteLine("Not valid command");
+                            break;
+                        }
+            }
+            Console.ReadKey();
+        }     
+        
+    }
+}
