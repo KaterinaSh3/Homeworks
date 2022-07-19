56521e83d324cc0b214b7d0ab34532acad86d7ae
Ekaterina Shevchenko <ekaterina.shevchenko@finteco.io>
2022-07-19 16:33:08 +0300

    add las hw

---
+++ Homework3/HoweWork3/HomeWork3numbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace HomeWork3numbers
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            if (a > 100 && b > 100)
+            { 
+                Console.WriteLine($"Each of the numbers A and B is more than 100"); 
+            }
+            if (a % 2 == 0 | b % 2 == 0)
+            {
+                Console.WriteLine("Only one of the numbers A and B is even"); //как написать только одно из?
+            }
+            if (a > 0 || b > 0)
+            {
+                Console.WriteLine($"One of the numbers A and B is positive");
+            }
+            if (a % 3 == 0 && b % 3 == 0 && c % 3 == 0)
+            {
+                Console.WriteLine($"Each of the numbers A, B, C is a multiple of three");
+            }
+            if (a < 50 | b < 50 | c < 50)
+            {
+                Console.WriteLine($"Only one of the numbers A, B, C is less than 50"); //как написать только одно из?
+            }
+            if (a < 0 | b < 0 | c < 0)
+            {
+                Console.WriteLine($"One of the numbers A, B, C is negative");
+            }
+
+        Console.ReadKey();
+        }
+    }
+}
---
+++ Homework3/HoweWork3/HoweWorkCalc/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace HomeWork3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Your Simple Calculator");
+            Console.WriteLine("----------------------------");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c;
+            char oper = char.Parse(Console.ReadLine());
+            switch (oper)
+            {
+                case '+':
+                    {
+                        c = a + b;
+                        Console.WriteLine(c);
+                        break;
+                    }
+                case '-':
+                    {
+                        c = a - b;
+                        Console.WriteLine(c);
+                        break;
+                    }
+                case '*':
+                    {
+                        c = a * b;
+                        Console.WriteLine(c);
+                        break;
+                    }
+                case '/':
+                    {
+                        c = a / b;
+                        Console.WriteLine(c);
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Not valid command");
+                        break;
+                    }
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
